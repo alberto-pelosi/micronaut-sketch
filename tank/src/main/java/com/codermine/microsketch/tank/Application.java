e7b86259b487ffa70169e8b9dfd5aa8fd9c3ad24
apelosi-bb <a.pelosi@codermine.com>
2020-01-20 18:25:11 +0100

    Openapit application annotation

--- tank/src/main/java/com/codermine/microsketch/tank/Application.java
+++ tank/src/main/java/com/codermine/microsketch/tank/Application.java
@@ -1,7 +1,16 @@
 package com.codermine.microsketch.tank;
 
 import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "micronautsketch",
+                version = "v1",
+
+                description = "That's one small step for [a] man")
+)
 public class Application {
 
     public static void main(String[] args) {
